7ac61bf3615839700ae6f602a83bf9f8ec7e576d
Oliver Cox <olivercoxdesign@gmail.com>
2022-06-10 22:40:09 +0100

    added colour variation for list block

--- components/builders/page-builder/components/List.vue
+++ components/builders/page-builder/components/List.vue
@@ -1,16 +1,23 @@
 <template>
-    <div class="px-4 px-0 my-10 lg:my-20 mx-auto container lg:max-w-6xl">
-        <div class="text-center mb-2 md:mb-5 lg:mb-10" v-if="block.heading">
-            <h2 class="font-heading text-4xl md:text-6xl lg:text-8xl text-slant uppercase text-brand-navy">{{ block.heading }}</h2>
-        </div>
-        <div class="flex flex-col md:flex-row justify-between">
-            <div class="usps pl-3 lg:pl-5 my-5 relative md:max-w-220 lg:max-w-300" v-for="item in block.listItem" :key="item.id">
-                <h3 class="font-anton mb-1 md:mb-2 text-xl md:text-2xl lg:text-4xl uppercase">{{ item.heading }}</h3>
-                <p class="font-body text-sm md:text-base lg:text-xl">{{ item.body }}</p>
+    <div :class="block.backgroundColour ? buildBackgroundColour() : null ">
+        <div class="px-4 px-0 mx-auto container lg:max-w-6xl" :class="block.backgroundColour ? 'py-10 lg:py-20' : 'my-10 lg:my-20'">
+            <div v-if="block.heading" class="text-center mb-2 md:mb-5 lg:mb-10">
+                <h2 class="font-heading text-4xl md:text-6xl lg:text-8xl text-slant uppercase " :class="block.backgroundColour ? 'text-brand-white' : 'text-brand-navy' ">
+                    {{ block.heading }}
+                </h2>
+            </div>
+            <div class="flex flex-col md:flex-row justify-between">
+                <div v-for="item in block.listItem" :key="item.id" class="usps pl-3 lg:pl-5 my-5 relative md:max-w-220 lg:max-w-300">
+                    <h3 class="font-anton mb-1 md:mb-2 text-xl md:text-2xl lg:text-4xl uppercase" :class="block.backgroundColour ? 'text-brand-white' : 'text-brand-black' ">
+                        {{ item.heading }}
+                    </h3>
+                    <p class="font-body text-sm md:text-base lg:text-xl" :class="block.backgroundColour ? 'text-brand-white' : 'text-brand-black' ">
+                        {{ item.body }}
+                    </p>
+                </div>
             </div>
         </div>
     </div>
-    
 </template>
 <script>
 export default {
@@ -18,6 +25,11 @@ export default {
         block: {
             type: Object,
             required: true
+        }
+    },
+    methods: {
+        buildBackgroundColour() {
+            return ' bg-brand-' + this.block.backgroundColour.toLowerCase()
         }
     }
 }
